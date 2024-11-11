d43d8d847ae45aacb5cc0c19331f057f32a527de
Tariq Abdullah <tariq@ideaquotient.in>
2024-11-11 20:57:55 +0530

    restructure

--- bootcomposer-converter/bootcomposer-coverter.php
+++ bootcomposer-converter/bootcomposer-coverter.php
@@ -16,6 +16,6 @@ define('MASTER_PLUGIN_DIR', plugin_dir_path(__FILE__));
 
 // Include each sub-plugin
 include_once MASTER_PLUGIN_DIR . 'enable-disable-features/enable-disable-features.php';
-include_once MASTER_PLUGIN_DIR . 'searchable-select/searchable-select.php';
+include_once MASTER_PLUGIN_DIR . 'searchable-select/searchable-select-in-wp-admin.php';
 include_once MASTER_PLUGIN_DIR . 'wp-pace/wp-pace.php';
 include_once MASTER_PLUGIN_DIR . 'admin-pro/admin-pro.php';
