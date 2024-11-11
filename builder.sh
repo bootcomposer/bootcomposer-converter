191fd5bca654e7d9ffe0042e691f9c074dbd346e
Tariq Abdullah <tariq@ideaquotient.in>
2024-11-11 20:32:37 +0530

    added plugin

--- builder.sh
+++ builder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Define the URLs and desired filenames
 URLS=(
-    "https://github.com/bootcomposer/wp-pace/archive/refs/tags/V1.0.zip wp-pace.zip"
-    "https://github.com/bootcomposer/admin-pro/archive/refs/tags/v1.0.zip admin-pro.zip"
-    "https://github.com/bootcomposer/searchable-select/archive/refs/tags/v1.0.zip searchable-select.zip"
-    "https://github.com/bootcomposer/enable-disable-features/archive/refs/tags/v1.0.zip enable-disable-features.zip"
+    "https://github.com/bootcomposer/wp-pace/archive/refs/tags/V1.0.zip wp-pace"
+    "https://github.com/bootcomposer/admin-pro/archive/refs/tags/v1.0.zip admin-pro"
+    "https://github.com/bootcomposer/searchable-select/archive/refs/tags/v1.0.zip searchable-select"
+    "https://github.com/bootcomposer/enable-disable-features/archive/refs/tags/v1.0.zip enable-disable-features"
 )
 
 # Create a directory for extracted files
@@ -13,20 +13,26 @@ mkdir -p "$EXTRACT_DIR"
 
 # Loop through each URL and filename
 for url_filename in "${URLS[@]}"; do
-    # Extract the URL and the desired filename
+    # Extract the URL and the desired directory name
     url=$(echo "$url_filename" | awk '{print $1}')
-    filename=$(echo "$url_filename" | awk '{print $2}')
+    dirname=$(echo "$url_filename" | awk '{print $2}')
+    # Define the target directory to extract files
+    target_dir="$EXTRACT_DIR/$dirname"
     
     # Download the .zip file
+    filename="${dirname}.zip"
     echo "Downloading $filename..."
     if ! wget -q "$url" -O "$filename"; then
         echo "Failed to download $filename"
         continue
     fi
     
-    # Unzip the file directly into the EXTRACT_DIR without subdirectories (using -j option)
+    # Create a directory for extraction if it doesn't exist
+    mkdir -p "$target_dir"
+    
+    # Unzip the file directly into the target directory without subdirectories (using -j option)
     echo "Unzipping $filename..."
-    if ! unzip -q -j "$filename" -d "$EXTRACT_DIR"; then
+    if ! unzip -q -j "$filename" -d "$target_dir"; then
         echo "Failed to unzip $filename"
         rm "$filename"
         continue
